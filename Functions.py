1dacfaaebd58969d1b9911dcb7aadb237d6a077d
Vincent Vargas <vincentvargas6969@gmail.com>
2022-08-04 02:13:16 -0700

    Initial Commit

---
+++ Functions.py
@@ -0,0 +1,8 @@
+def car (self, doors, make, model, engine_type, engine_size):
+    self.doors = doors
+    self.make = make
+    self.model = model
+    self.engine_type = engine_type
+    self.engine_size = engine_size
+
+car()
